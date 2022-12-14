4a14e8327aade73e0bebc9ff66160217ff5763bf
richiejabel <henrichjabel@gmail.com>
2022-12-14 08:48:29 -0800

    print 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,12 @@ void print_alphabet_x10(void)
 	char small_letters;
 	int num;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 	for (small_letters = 'a'; small_letters <= 'z'; small_letters++)
 		_putchar(small_letters);
 	_putchar('\n');
 	{
-		
 	if (num < 10)
 		continue;
 	}
