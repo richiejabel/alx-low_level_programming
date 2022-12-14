ee0d9f35e59abb313f85eaa165039346652d82f3
richiejabel <henrichjabel@gmail.com>
2022-12-14 10:30:04 -0800

    absulute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of integer
+ * int: integer
+ * @num: number being checked
+ * Return: Always 0
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+
+		_putchar(-1 * num);
+
+	else
+		_putchar(num);
+
+}
