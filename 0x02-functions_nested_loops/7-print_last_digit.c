74f9111d57ffce650fa623f1ad2bcaaea3c90733
richiejabel <henrichjabel@gmail.com>
2022-12-14 10:45:52 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit of number
+ * @dig: full number
+ * Return: Returns the last digit
+ */
+
+int print_last_digit(int dig)
+
+{
+	return (dig % 10);
+
+}
