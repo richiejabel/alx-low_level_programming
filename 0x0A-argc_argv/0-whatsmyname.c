5893a2616f1baea14c001c6f4c15c494a2311b4e
richiejabel <henrichjabel@gmail.com>
2023-01-15 12:06:17 -0800

    my own

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - argument
+ *Return: always 0
+ *@argc: count argurments
+ *@argv: arguments vector
+ */
+
+int main(int argc, char **argv)
+{
+		printf("%s\n", argv[0]);
+	return (0);
+}
