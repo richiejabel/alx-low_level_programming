6c70e8b8e79486d9b7df772059e2f0b95884c728
richiejabel <henrichjabel@gmail.com>
2023-01-17 11:09:20 -0800

    trouble

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of integer
+ * int: integer
+ * @num: number being checked
+ * Return: Always 0
+ */
+
+int _abs(int num)
+{
+	if (num >= 0)
+
+		return (num);
+
+	else
+		return (-num);
+}
